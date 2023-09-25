5b59f7515dc7cbd306f041e02a6a08ac73884614
Michael Harris <xero5k@yahoo.com>
2023-09-25 09:23:16 -0500

    refreshing ruby

---
+++ app.rb
@@ -0,0 +1,12 @@
+#how to comment
+#ruby : Language
+#Integer
+#Float
+#String
+#Boolean
+#Symbol
+#Hash
+#Array
+#All Capitalized is constant: "CAT"
+#lower case is local variable: "cat"
+#dollar sign is a global variable: "$cat"
