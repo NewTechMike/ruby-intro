2b7204b10bea989583d3254a8a90a445569f8848
Michael Harris <xero5k@yahoo.com>
2023-09-29 09:50:39 -0500

    More ruby basics

--- app.rb
+++ app.rb
@@ -10,3 +10,20 @@
 #All Capitalized is constant: "CAT"
 #lower case is local variable: "cat"
 #dollar sign is a global variable: "$cat"
+
+dog_name = "Lucy"
+puts "Say hello to my dog #{dog_name}"
+
+uc = dog_name.upcase
+puts "#{uc}"
+lc = uc.downcase
+puts "#{lc}"
+cap = lc.capitalize 
+puts "#{cap}"
+rev = cap.reverse
+puts "#{rev}"
+hw = "hello" + "world"
+puts "#{hw}"
+hi3 = "hello " * 3
+puts "#{hi3}"
+
