b15f39f18ce9ebe3f7d8bce3ad15f2b8c6aee462
Michael Harris <xero5k@yahoo.com>
2023-10-02 20:43:11 -0500

    continuing ruby basics

--- app.rb
+++ app.rb
@@ -27,3 +27,33 @@ puts "#{hw}"
 hi3 = "hello " * 3
 puts "#{hi3}"
 
+str1 = "hello"
+str2 = "there"
+puts str1 + str2
+
+7.5.floor
+7.5.ceil
+10.next
+
+"1".to_i
+"1.1".to_i
+"1.1".to_f
+
+4/3
+4/3.0
+4/3.to_f
+
+true.class
+false.class
+
+puts :my_symbol.object_id
+puts "my_string".object_id
+puts "my_string".object_id
+
+Array.new
+[5, 100, 234, 7, 2].sort
+[3, 2, 1].reverse
+
+my_hash = { key1: "Value1", key2: "Value2"}
+
+puts my_hash[:key2]
