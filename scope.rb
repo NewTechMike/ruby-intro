607f203b6f28744f2821200cb275f9e5c9db4082
Michael Harris <xero5k@yahoo.com>
2023-10-20 17:28:55 -0500

    Scope again

--- scope.rb
+++ scope.rb
@@ -8,10 +8,21 @@ end
 
 greeting(" ")
 
-evil_monster = "Bowser"
+$evil_monster = "Bowser"
 
-def princess_peaches_castle(evil_monster)
-  puts "#{evil_monster} is trying to kidnap Princess Peach"
+def princess_peaches_castle
+  puts "#{$evil_monster} is trying to kidnap Princess Peach"
 end
 
-princess_peaches_castle(evil_monster)
+princess_peaches_castle
+
+#Variables
+#Local: lower case or underscore
+name = "Lucy"
+#Global: dollar sign
+$name = "Lucy"
+#Instance: At Symbol
+@name = "Lucy"
+#Class: two At Symbols
+@@name = "Lucy"
+
