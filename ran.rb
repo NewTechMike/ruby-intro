af558fa8e0ae168f8640992c1baef533a31e9508
Michael Harris <xero5k@yahoo.com>
2023-10-24 08:19:33 -0500

    Ruby Conditionals

--- ran.rb
+++ ran.rb
@@ -1,10 +1,46 @@
 require "pry"
 
-def my_name name
-  binding.pry
-  "hi, my name is #{name}"
+#def my_name name
+#  binding.pry
+#  "hi, my name is #{name}"
+#end 
+
+#my_name("Rose")
+#my_name("Karl")
+#my_name("Sora")
+
+
+#Conditional
+def to_do day
+  if day == "Monday"
+    "Pick up dry cleaning"
+  else 
+    "Take cat to vet"
+  end 
 end 
 
-my_name("Rose")
-my_name("Karl")
-my_name("Sora")
+def do_dishes dishes 
+  unless dishes == "clean"
+    "run dishwasher"
+  else dishes == "dirty"
+    "Do not run dishwasher"
+  end 
+end 
+
+
+def to_do_week day 
+  case day 
+  when "Monday"
+    "Pickup dry cleaning"
+  when "Tuesday"
+      "Buy Groceries"
+  when "Wednesday"
+    "Do Laundry"
+  when "Thursday"
+    "Go Run"
+  else 
+    "Game all the time"
+  end 
+end
+
+binding.pry
