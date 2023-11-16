d8fef88ebb70f32656d0add1cf72811fbd04bfe7
Michael Harris <xero5k@yahoo.com>
2023-11-16 11:56:22 -0600

    Hashes

---
+++ hash.rb
@@ -0,0 +1,21 @@
+require "pry"
+
+h1 = {key1: "value 1", key2: "value 2"}
+
+
+user = {
+    name: "Duane",
+    address: {
+      street: "123 fake street",
+      city: "Queens",
+      state: "NY"
+    }
+}
+
+pizza_toppings = {
+  cheese: true,
+  topping1: "Mushroom",
+  topping2: "Sausage"
+}
+
+binding.pry
