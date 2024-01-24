3fa70cc4a8b5bfea4e947138fccebde6d5ee2554
Michael Harris <xero5k@yahoo.com>
2024-01-24 08:58:47 -0600

    setting up new array methods

--- count.rb
+++ count.rb
@@ -26,3 +26,24 @@ def create_an_empty_array
 end
 
 create_an_empty_array
+
+def add_element_to_end_of_array
+end
+
+def add_element_to_start_of_array
+end
+
+def remove_element_from_end_of_array
+end
+
+def remove_element_from_start_of_array
+end
+
+def retrieve_element_from_index
+end
+
+def retrieve_first_element_from_array
+end
+
+def retrieve_last_element_from_array
+end
