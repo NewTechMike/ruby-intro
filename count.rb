15465105f7a73537bbdeba0558ce6b7aa229d2ce
Michael Harris <xero5k@yahoo.com>
2024-01-29 19:55:47 -0600

    Quick methods

--- count.rb
+++ count.rb
@@ -25,25 +25,57 @@ def create_an_empty_array
  return test
 end
 
-create_an_empty_array
+#create_an_empty_array
 
-def add_element_to_end_of_array
+def add_element_to_end_of_array(arr, e)
+  #takes an array and an element
+  #Use shovel or push to add Element to end of array
+  #binding.pry
+  return arr << e
 end
 
-def add_element_to_start_of_array
+small = [1, 2, 3, 4]
+num = 5
+#puts add_element_to_end_of_array(small, num)
+
+def add_element_to_start_of_array(arr, e)
+  #same as above, use unshift to add to beginning
+  #binding.pry
+  arr.unshift(e)
 end
 
-def remove_element_from_end_of_array
+#puts add_element_to_start_of_array(small, num)
+
+def remove_element_from_end_of_array(arr)
+  #takes an array, removes last element with pop
+  #binding.pry
+  arr.pop
 end
 
-def remove_element_from_start_of_array
+#puts remove_element_from_end_of_array(small)
+
+def remove_element_from_start_of_array(arr)
+  #same as above but removes from beginning with shift
+  #binding.pry
+  arr.shift
 end
 
-def retrieve_element_from_index
+#puts remove_element_from_start_of_array(small)
+
+def retrieve_element_from_index(arr, i)
+  #takes an array and index from which to retrieve from
+  #binding.pry
+  arr[i]
 end
 
-def retrieve_first_element_from_array
+#puts retrieve_element_from_index(small, 2)
+
+def retrieve_first_element_from_array(arr)
+  arr[0]
 end
 
-def retrieve_last_element_from_array
+def retrieve_last_element_from_array(arr)
+  arr[(arr.length-1)]
 end
+
+puts retrieve_last_element_from_array(small)
